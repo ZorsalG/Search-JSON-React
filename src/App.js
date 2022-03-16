94b43a572f06cec4efe8c45df67927c3d0a4b80d
ZorsalG <alejandrofal98@gmail.com>
2022-03-16 09:31:10 +0100

    Buscador funcional

--- src/App.js
+++ src/App.js
@@ -2,41 +2,45 @@ import React from "react";
 import ListData from "./components/ListData.json"
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            search: null
-        };
+
+    constructor(){
+      super();
+      this.state={
+        search:null
+      };
     }
 
-    search = (e) => {
-        let keyword = e.target.value;
-        this.setState({ search: keyword })
-    }
+    search=(event)=>{
+        let keyword = event.target.value;
+        this.setState({search:keyword})
+      }
 
-    render() {
-        const items = ListData.filter((data) => {
-            if (this.state.search == null)
+    render(){
+        const items = ListData.filter((data)=>{
+            if(this.state.search == null)
                 return data
-        }).map(data => {
-            return (
-                <div>
-                    <ul>
-                        <li>
-                            <span>{data.name}</span>
-                        </li>
-                    </ul>
-                </div>
-            )
-        })
-
-        return (
-            <div>
-                <input type="text" onChange={(e) => this.search(e)} />
-                {items}
-            </div>
+            else if(data.name.toLowerCase().includes(this.state.search.toLowerCase()) || data.email.toLowerCase().includes(this.state.search.toLowerCase())){
+                return data
+            }
+          }).map(data=>{
+            return(
+        <div>
+          <ul>
+            <li>
+              <span>{data.name} ({data.email})</span>
+            </li>
+          </ul>
+        </div>
         )
+      })
+  
+      return (
+        <div>
+        <input type="text" onChange={(e)=>this.search(e)} />
+        {items}
+        </div>
+      )
     }
-}
-
+  }
+  
 export default App;
